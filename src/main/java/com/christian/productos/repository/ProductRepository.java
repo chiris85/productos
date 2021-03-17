3f81c9b750f628350675145061b91176a8584e22
Christian Acevedo <christian_acevedo@hotmail.com>
2021-03-16 21:12:38 -0500

    The JPA interface repository was created

--- src/main/java/com/christian/productos/repository/ProductRepository.java
+++ src/main/java/com/christian/productos/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.christian.productos.repository;
 
-public class ProductRepository {
+import com.christian.productos.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
